c1cc48e41c960a05b18ebfe104ff9d26b84c1bb0
Jack Hayward <38814148+JackHayward@users.noreply.github.com>
2020-07-15 16:07:08 +0100

    Application now displays sandwiches as expected, and includes a scrollview and error handling for null values.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,23 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+import java.util.ArrayList;
+import java.util.List;
 import org.json.JSONException;
+
+import static android.view.View.GONE;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private TextView descriptionTv;
+    private TextView alsoKnownAsTv;
+    private TextView alsoKnownAsLabel;
+    private TextView ingredientsTv;
+    private TextView placeOfOriginTv;
+    private TextView placeOfOriginLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        descriptionTv = findViewById(R.id.description_tv);
+        alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        alsoKnownAsLabel = findViewById(R.id.also_known_label);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        placeOfOriginTv = findViewById(R.id.origin_tv);
+        placeOfOriginLabel = findViewById(R.id.origin_label);
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
@@ -49,12 +67,11 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
-        setTitle(sandwich.getMainName());
+        populateUI(sandwich);
     }
 
     private void closeOnError() {
@@ -62,7 +79,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        setTitle(sandwich.getMainName());
 
+        if (sandwich.getAlsoKnownAs().size() == 0) {
+            alsoKnownAsTv.setVisibility(GONE);
+            alsoKnownAsLabel.setVisibility(GONE);
+        } else {
+            populateTextViewWithArrayList(alsoKnownAsTv, sandwich.getAlsoKnownAs());
+        }
+
+        if (sandwich.getPlaceOfOrigin().equals("")) {
+            placeOfOriginTv.setVisibility(GONE);
+            placeOfOriginLabel.setVisibility(GONE);
+        } else {
+            placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        descriptionTv.setText(sandwich.getDescription());
+        populateTextViewWithArrayList(ingredientsTv, sandwich.getIngredients());
+    }
+
+    private void populateTextViewWithArrayList(TextView textView, List<String> list) {
+        String listString = "";
+
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i) == list.get(list.size() -1)){
+                listString += "- " + list.get(i);
+            } else {
+                listString += "- " + list.get(i) + "\n";
+            }
+        }
+        textView.setText(listString);
     }
 }
